642c827ce5e6f0f68c9e2189e9d8d418f3a7ddd6
Petar Iliev <petar.iliev@servicenow.com>
2023-07-08 13:06:12 -0700

    Initial commit

---
+++ Africa/App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Africa
+//
+//  Created by Petar Iliev on 7/8/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Content")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
