2dbf6a36611d6dedbf1a670d76f73c563348661a
Petar Iliev <petar.iliev@servicenow.com>
2023-07-08 12:48:05 -0700

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Petar Iliev on 7/8/23.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
